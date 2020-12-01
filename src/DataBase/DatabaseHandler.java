f45bd5012f35f1f77334f2f975c619c039030d2e
Gomerok <danila.gomerov@gmail.com>
2020-12-01 04:39:04 +0300

    SignIn

--- src/DataBase/DatabaseHandler.java
+++ src/DataBase/DatabaseHandler.java
@@ -2,54 +2,74 @@ package DataBase;
 
 import java.sql.PreparedStatement;
 import java.sql.*;
+import java.util.ArrayList;
 
-public class DatabaseHandler extends Configs{
+public class DatabaseHandler extends Configs {
 
-  Connection dbConnection;
+    Connection dbConnection;
 
-  public Connection getDbConnection(){
-      String url = "jdbc:mysql://localhost:3306/terminal?serverTimezone=UTC";
-      try {
-          Class.forName("com.mysql.cj.jdbc.Driver");
-          dbConnection=DriverManager.getConnection(url,dbUser,dbPass);
-      } catch (ClassNotFoundException e) {
-          e.printStackTrace();
-      } catch (SQLException e1) {
-          e1.printStackTrace();
-      }
-      return  dbConnection;
-  }
-
-public void signUpUser(String name,String surname,String login,String password){
-  String insert = "INSERT INTO "+ Const.USER_TABLE+
-          "("+Const.USERS_NAME+","+Const.USERS_SURNAME+","+Const.USERS_LOGIN+","+Const.USERS_PASSWORD+")"+"VALUES(?,?,?,?)";
-
-    try {
-        PreparedStatement prSt = getDbConnection().prepareStatement(insert);
-
-    prSt.setString(1, name);
-    prSt.setString(2, surname);
-    prSt.setString(3, login);
-    prSt.setString(4, password);
-    prSt.executeUpdate();
-    prSt.close();
-
-    } catch (SQLException e2) {
-        e2.printStackTrace();
+    public Connection getDbConnection() {
+        String url = "jdbc:mysql://localhost:3306/terminal?serverTimezone=UTC";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            dbConnection = DriverManager.getConnection(url, dbUser, dbPass);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
+        return dbConnection;
     }
 
-}
+    public void signUpUser(String name, String surname, String login, String password) {
+        String insert = "INSERT INTO " + Const.USER_TABLE +
+                "(" + Const.USERS_NAME + "," + Const.USERS_SURNAME + "," + Const.USERS_LOGIN + "," + Const.USERS_PASSWORD + ")" + "VALUES(?,?,?,?)";
 
-    public String SelectUser(String insert){
         try {
-            Statement St = getDbConnection().createStatement();
-            ResultSet rs = St.executeQuery(insert);
-String Name = rs.getString(1);
-            return Name;
+            PreparedStatement prSt = getDbConnection().prepareStatement(insert);
+
+            prSt.setString(1, name);
+            prSt.setString(2, surname);
+            prSt.setString(3, login);
+            prSt.setString(4, password);
+            prSt.executeUpdate();
+            prSt.close();
+
         } catch (SQLException e2) {
             e2.printStackTrace();
         }
 
+    }
+
+    public ArrayList<String> SelectUser(String insert, int n, String namer) {
+
+        ArrayList<String> uspas = new ArrayList<>();
+
+
+        try {
+            if (n == 1) {
+                PreparedStatement prepStat = getDbConnection().prepareStatement(insert);
+                ResultSet rs = prepStat.executeQuery();
+                while (rs.next()) {
+                    uspas.add(0, rs.getString("name"));
+                    uspas.add(1, rs.getString("password"));
+                }
+            } else {
+                PreparedStatement preparedStatement = getDbConnection().prepareStatement("SELECT name,password FROM terminal.users where (name=?)");
+                preparedStatement.setString(1, namer);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                while (resultSet.next()) {
+                    uspas.add(0, resultSet.getString("name"));
+                    uspas.add(1, resultSet.getString("password"));
+                }
+            }
+        } catch (SQLException e2) {
+            e2.printStackTrace();
+        }
+        System.out.println(uspas.get(0));
+        System.out.println(uspas.get(1));
+
+        return uspas;
     }
 
 }
--- src/sample/SignInController.java
+++ src/sample/SignInController.java
@@ -3,7 +3,9 @@ package sample;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -38,62 +40,67 @@ public class SignInController {
     @FXML
     void initialize() {
         DatabaseHandler dbHandler = new DatabaseHandler();
-        String insert= "SELECT name FROM terminal.users where (idusers=33)";
-        String  NNNN= dbHandler.SelectUser(insert);
-        System.out.println(NNNN);
-System.out.println(insert);
+
         SignInButton.setOnAction(actionEvent -> {
-           SignInButton.getScene().getWindow().hide();
-           FXMLLoader loader = new FXMLLoader();
 
-           String name = "admin";
-           String pass = "12345678";
-           if(login_field.getText().equals(name) && password_field.getText().equals(pass)){
-               System.out.println(login_field.getText());
-               System.out.println(password_field.getText());
-               //FXMLLoader loader = new FXMLLoader();
-               loader.setLocation(getClass().getResource("/Admin/admin_menu.fxml"));
+            String insert = "SELECT name,password FROM terminal.users where (idusers=38)";
+            String insert2 = "SELECT name,password FROM terminal.users where (name=?)";
 
-               try {
-                   loader.load();
-               } catch (IOException e) {
-                   e.printStackTrace();
-               }
-               Parent root = loader.getRoot();
-               Stage stage = new Stage();
-               stage.setScene(new Scene(root));
-               stage.showAndWait();
-           }else{
-               loader.setLocation(getClass().getResource("/Cashier/cashier_menu.fxml"));
-               try {
-                   loader.load();
-               } catch (IOException e) {
-                   e.printStackTrace();
-               }
-               Parent root = loader.getRoot();
-               Stage stage = new Stage();
-               stage.setScene(new Scene(root));
-               stage.showAndWait();
-           }
+            ArrayList<String> UsePass = new ArrayList<>();
+            UsePass = dbHandler.SelectUser(insert, 1, null);
 
-       });
+            String Pass = UsePass.get(1);
+            String Name = UsePass.get(0);
 
-        SignUpButtonV.setOnAction(Event -> {
             SignInButton.getScene().getWindow().hide();
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(getClass().getResource("/Admin/create_cashier.fxml"));
-
             try {
-                loader.load();
+                if (login_field.getText().equals(Name) && password_field.getText().equals(Pass)) {
+                    loader.setLocation(getClass().getResource("/Admin/admin_menu.fxml"));
+
+                    loader.load();
+
+                    Parent root = loader.getRoot();
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(root));
+                    stage.showAndWait();
+                } else {
+                    ArrayList<String> rew = new ArrayList<>();
+                    rew = dbHandler.SelectUser(insert2, 2, login_field.getText());
+                    String Name1 = rew.get(0);
+                    String Pass1 = rew.get(1);
+                    if (login_field.getText().equals(Name1) && password_field.getText().equals(Pass1)) {
+                        loader.setLocation(getClass().getResource("/Cashier/cashier_menu.fxml"));
+                    }
+
+                    loader.load();
+
+                    Parent root = loader.getRoot();
+                    Stage stage = new Stage();
+                    stage.setScene(new Scene(root));
+                    stage.showAndWait();
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            Parent root = loader.getRoot();
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.showAndWait();
-
         });
+
+//        SignUpButtonV.setOnAction(Event -> {
+//            SignInButton.getScene().getWindow().hide();
+//            FXMLLoader loader = new FXMLLoader();
+//            loader.setLocation(getClass().getResource("/Admin/create_cashier.fxml"));
+//
+//            try {
+//                loader.load();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            Parent root = loader.getRoot();
+//            Stage stage = new Stage();
+//            stage.setScene(new Scene(root));
+//            stage.showAndWait();
+//
+//        });
 
 
     }
